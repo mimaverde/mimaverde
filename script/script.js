987e13d185e807e19523532ad8f6d8b3acde2d53
mimaverde <verdemima@gmail.com>
2024-10-09 16:36:36 -0300

    Contact form with fields and sending email

--- script/script.js
+++ script/script.js
@@ -115,5 +115,38 @@ document.addEventListener('DOMContentLoaded', () => {
     menuNav.addEventListener('click', function() {
         togglenav.click();
     });
+
+    //Send the contact email
+    const form = document.getElementById("myContact");
+  
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const status = document.getElementById("myStatus");
+        const data = new FormData(event.target);
+
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+        }
+        }).then(response => {
+            if (response.ok) {
+                status.innerHTML = "Thanks for your submission!";
+                form.reset();
+            } else {
+                response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form"
+                }
+                })
+            }
+        }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+        });
+    }
+    form.addEventListener("submit", handleSubmit)
 });
 
